70f54a37e4dc47608292ae899e4d2c6a438ebb5d
Armin Grodon <armin.grodon@googlemail.com>
2016-11-18 20:29:36 +0100

    fix remote path in update-amazon.sh

--- update-amazon.sh
+++ update-amazon.sh
@@ -4,13 +4,13 @@ SSH="elec"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PLUGIN=plugin.video.amazon-test
-TAR=$DIR/$PLUGIN.tar.gz
+TAR=$PLUGIN.tar.gz
 KODI=.kodi/addons
 
-tar -czf $TAR -C $DIR/xbmc-amazon/packages/ $PLUGIN
-scp $TAR elec:.
+tar -czf $DIR/$TAR -C $DIR/xbmc-amazon/packages/ $PLUGIN
+scp $DIR/$TAR elec:.
 ssh $SSH rm -rf $KODI/$PLUGIN
 ssh $SSH tar -xzf $TAR -C $KODI
 ssh $SSH rm $TAR
 ssh $SSH unzip -d $KODI -o $KODI/$PLUGIN/*.zip
-rm $TAR
+rm $DIR/$TAR
