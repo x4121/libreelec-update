bd290409ad07108bec5cbb4ad17590e921393805
Armin Grodon <armin.grodon@googlemail.com>
2016-11-19 13:46:52 +0100

    replace absolute path with pushd/popd

--- update-kodi.sh
+++ update-kodi.sh
@@ -6,7 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 URL=http://milhouse.libreelec.tv/builds/master/RPi2/
 VERSION=kodi.version
 
-CURRENT=`cat $DIR/$VERSION`
+pushd $DIR >&- 2>&-
+
+CURRENT=`cat $VERSION`
 NEXT=`curl -sf $URL | tail -2 | head -1 | sed 's/^.*href=\"\(.*\)\".*$/\1/'`
 
 if [ $? != 0 ]; then
@@ -16,7 +18,9 @@ fi
 if [ "$CURRENT" != "$NEXT" ]; then
     ssh $SSH curl -# -o .update/$NEXT $URL$NEXT
     ssh $SSH '{ sleep 1; reboot; } > /dev/null &'
-    echo $NEXT > $DIR/$VERSION
+    echo $NEXT > $VERSION
 else
     echo "already up to date"
 fi
+
+popd >&- 2>&-
